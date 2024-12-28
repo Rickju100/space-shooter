32d94409492cae971a175243b54301a256de921f
Rickju100 <ricardo.funez95@gmail.com>
2024-12-27 19:31:32 -0600

    1st

---
+++ code/shootingStars.py
@@ -0,0 +1,7 @@
+""""Importing the necessary libraries"""
+#Import
+import pygame
+import random
+from os import join as jn
+#Importing Images
+lase
---
+++ code/test.py
@@ -0,0 +1,3 @@
+with open("code/GAME.py", "rb") as f:
+    content = f.read()
+print(content.decode("utf-8", errors="replace"))
